f274e7a514d06e681627803d564f7d4f13bdd541
MathsHouse <mathshouse@gmail.com>
2023-12-13 20:17:30 +0200

    New update in style

--- pages/Login-Sign page/registration-script.js
+++ pages/Login-Sign page/registration-script.js
@@ -1,14 +1,17 @@
+// Varibel to registration form container && registration Button;
 const btn_log = document.getElementById("login-btn");
 const btn_sig = document.getElementById("signup-btn");
 const sig_form = document.getElementById("signup-form");
 const log_form = document.getElementById("login-form");
 
+//Event to set||remove class in element at sign form;
 btn_sig.addEventListener("click", function () {
   this.classList.add("active");
   btn_log.classList.remove("active");
   sig_form.style.display = "block";
   log_form.style.display = "none";
 });
+//Event to set||remove class in element at sign form;
 btn_log.addEventListener("click", function () {
   this.classList.add("active");
   btn_sig.classList.remove("active");
@@ -16,11 +19,14 @@ btn_log.addEventListener("click", function () {
   log_form.style.display = "flex";
 });
 
+//Varibel to select user kind;
 const user_kind = document.getElementById("user-kind");
 const lawyer_kind = document.getElementById("lawyer-kind");
 
+//Varibel to select lawyer categry;
 const lawyer_categry_sel = document.getElementById("lawyer-categry-sel");
 
+//Event to set||remove class in element at sign form;
 lawyer_kind.addEventListener("click", function () {
   user_kind.classList.remove("active-kind");
   this.classList.add("active-kind");
@@ -28,6 +34,8 @@ lawyer_kind.addEventListener("click", function () {
   user_kind.removeAttribute("name");
   lawyer_categry_sel.style.display = "block";
 });
+
+//Event to set||remove class in element at sign form;
 user_kind.addEventListener("click", function () {
   lawyer_kind.classList.remove("active-kind");
   this.classList.add("active-kind");
@@ -36,9 +44,11 @@ user_kind.addEventListener("click", function () {
   lawyer_categry_sel.style.display = "none";
 });
 
+//Varibel to select button to change form content;
 const spa_login = document.getElementById("spa-login");
 const spa_sign = document.getElementById("spa-sign");
 
+//Event to set||remove class in element at login form;
 spa_login.addEventListener("click", function () {
   btn_log.classList.add("active");
   btn_sig.classList.remove("active");
@@ -46,6 +56,7 @@ spa_login.addEventListener("click", function () {
   log_form.style.display = "flex";
 });
 
+//Event to set||remove class in element at sign form;
 spa_sign.addEventListener("click", function () {
   btn_sig.classList.add("active");
   btn_log.classList.remove("active");
@@ -53,16 +64,20 @@ spa_sign.addEventListener("click", function () {
   log_form.style.display = "none";
 });
 
+//Varibel to select password input to show || hide value in sign form;
 const show_pass_btn = document.getElementById("show_pass_sign");
 const hide_pass_btn = document.getElementById("hide_pass_sign");
 const pass_btn = document.getElementById("password_sign");
 
+//Event to show value in password button at sign form;
 show_pass_btn.addEventListener("click", function () {
   pass_btn.removeAttribute("type");
   pass_btn.setAttribute("type", "text");
   this.style.display = "none";
   hide_pass_btn.style.display = "block";
 });
+
+//Event to hide value in password button at sign form;
 hide_pass_btn.addEventListener("click", function () {
   pass_btn.removeAttribute("type");
   pass_btn.setAttribute("type", "password");
@@ -78,16 +93,20 @@ pass_btn.addEventListener("focusout", function () {
   }
 });
 
+//Varibel to select password input to show || hide value in loign form;
 const show_pass_loginBtn = document.getElementById("show_pass_login");
 const hide_pass_loginBtn = document.getElementById("hide_pass_login");
 const pass_loginBtn = document.getElementById("password_login");
 
+//Event to show value in password button at login form; 
 show_pass_loginBtn.addEventListener("click", function () {
   pass_loginBtn.removeAttribute("type");
   pass_loginBtn.setAttribute("type", "text");
   this.style.display = "none";
   hide_pass_loginBtn.style.display = "block";
 });
+
+//Event to hide value in password button at login form;
 hide_pass_loginBtn.addEventListener("click", function () {
   pass_loginBtn.removeAttribute("type");
   pass_loginBtn.setAttribute("type", "password");
@@ -103,9 +122,11 @@ pass_loginBtn.addEventListener("focusout", function () {
   }
 });
 
+//Varibel to select submit button in sign && login form;
 const sub_sign = document.getElementById("sub-signBtn");
 const sub_login = document.getElementById("sub-loginBtn");
 
+//Varibel to select * elemant validation from button at sign form;
 const fn_valid = document.getElementById("fn-valid");
 const ln_valid = document.getElementById("ln-valid");
 const email_valid = document.getElementById("email-valid");
@@ -113,10 +134,12 @@ const num_valid = document.getElementById("num-valid");
 const idintity_valid = document.getElementById("idintity-valid");
 const pass_valid = document.getElementById("pass-valid");
 
+//Varibel to select * elemant validation from button at login form;
 const email_login_valid = document.getElementById("email_login_valid");
 const pass_login_valid = document.getElementById("pass_login_valid");
 const email_login = document.getElementById("email_login_btn");
 
+//Varibel to select * value from button at sign form;
 const fn_btn = document.getElementById("fn-btn");
 const ln_btn = document.getElementById("ln-btn");
 const email_btn = document.getElementById("email-btn");
@@ -124,6 +147,49 @@ const num_btn = document.getElementById("num-btn");
 const idintity_btn = document.getElementById("idintity-btn");
 const pass_sign_btn = document.getElementById("password_sign");
 
+//Event Condition in first name button if he have a value || not at sign form;
+fn_btn.addEventListener("keyup", function () {
+  if (fn_btn.value !== "") {
+    fn_valid.style.display = "none";
+  }
+});
+
+//Event Condition in last name button if he have a value || not at sign form;
+ln_btn.addEventListener("keyup", function () {
+  if (ln_btn.value !== "") {
+    ln_valid.style.display = "none";
+  }
+});
+
+//Event Condition in email button if he have a value || not at sign form;
+email_btn.addEventListener("keyup", function () {
+  if (email_btn.value !== "") {
+    email_valid.style.display = "none";
+  }
+});
+
+//Event Condition in phone number button if he have a value || not at sign form;
+num_btn.addEventListener("keyup", function () {
+  if (num_btn.value !== "") {
+    num_valid.style.display = "none";
+  }
+});
+
+//Event Condition in idintity button if he have a value || not at sign form;
+idintity_btn.addEventListener("keyup", function () {
+  if (idintity_btn.value !== "") {
+    idintity_valid.style.display = "none";
+  }
+});
+
+//Event Condition in password button if he have a value || not at sign form;
+pass_sign_btn.addEventListener("keyup", function () {
+  if (pass_sign_btn.value !== "") {
+    pass_valid.style.display = "none";
+  }
+});
+
+//Event Condition to make my validation on element at sign form;
 sub_sign.addEventListener("click", function () {
   if (fn_btn.value == "") {
     fn_valid.style.display = "block";
@@ -155,42 +221,27 @@ sub_sign.addEventListener("click", function () {
     pass_valid.style.display = "block";
   }
 });
-fn_btn.addEventListener("keyup", function () {
-  if (fn_btn.value !== "") {
-    fn_valid.style.display = "none";
-    console.log("nooooo");
+
+//Event Condition in email button at login form;
+email_login.addEventListener("keyup", function () {
+  if (email_login.value !== "") {
+    email_login_valid.style.display = "none";
   }
 });
-ln_btn.addEventListener("keyup", function () {
-  if (ln_btn.value !== "") {
-    ln_valid.style.display = "none";
-    console.log("nooooo");
+
+//Event Condition in password button at login form;
+pass_loginBtn.addEventListener("keyup", function () {
+  if (this.value !== "") {
+    pass_login_valid.style.display = "none";
+  }
+  if (pass_loginBtn.value.length > 8) {
+    pass_login_valid.style.display = "none";
+  } else {
+    pass_login_valid.style.display = "block";
   }
 });
-email_btn.addEventListener("keyup", function () {
-  if (email_btn.value !== "") {
-    email_valid.style.display = "none";
-    console.log("nooooo");
-  }
-});
-num_btn.addEventListener("keyup", function () {
-  if (num_btn.value !== "") {
-    num_valid.style.display = "none";
-    console.log("nooooo");
-  }
-});
-idintity_btn.addEventListener("keyup", function () {
-  if (idintity_btn.value !== "") {
-    idintity_valid.style.display = "none";
-    console.log("nooooo");
-  }
-});
-pass_sign_btn.addEventListener("keyup", function () {
-  if (pass_sign_btn.value !== "") {
-    pass_valid.style.display = "none";
-    console.log("nooooo");
-  }
-});
+
+//Event Condition to make my validation on element at login form;
 sub_login.addEventListener("click", function () {
   if (email_login.value == "") {
     email_login_valid.style.display = "block";
@@ -201,21 +252,6 @@ sub_login.addEventListener("click", function () {
     email_login_valid.style.display = "block";
   }
   if (pass_loginBtn.value == "") {
-    pass_login_valid.style.display = "block";
-  }
-});
-email_login.addEventListener("keyup", function () {
-  if (email_login.value !== "") {
-    email_login_valid.style.display = "none";
-  }
-});
-pass_loginBtn.addEventListener("keyup", function () {
-  if (this.value !== "") {
-    pass_login_valid.style.display = "none";
-  }
-  if (pass_loginBtn.value.length > 8) {
-    pass_login_valid.style.display = "none";
-  } else {
     pass_login_valid.style.display = "block";
   }
 });
