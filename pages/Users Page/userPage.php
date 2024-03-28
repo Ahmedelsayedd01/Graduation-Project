44b6fb670da06d039d11a805ef421d3c84103e9f
ahmedelsayedd01 <ahmedelsayed201972@gmail.com>
2024-03-28 05:25:22 +0200

    Finish Fetch Post User to Page A

--- pages/Users Page/userPage.php
+++ pages/Users Page/userPage.php
@@ -23,21 +23,15 @@ if (!isset($_SESSION['user'])) {
   <!-- Icons Liberary -->
   <script src="https://kit.fontawesome.com/bbda8ae88d.js" crossorigin="anonymous"></script>
   <!-- Bootstrap Liberary -->
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
-    integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous" />
-  <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
-    integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous" />
+  <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous">
   </script>
-  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js"
-    integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous">
+  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous">
   </script>
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js"
-    integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous">
   </script>
   <!-- Jquery Liberary -->
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js"
-    integrity="sha512-v2CJ7UaYy4JwqLDIrZUI/4hqeoQieOmAZNXBeQyjo21dadnwR+8ZaIJVT8EE2iyI61OV8e6M8PP2/4hpQINQ/g=="
-    crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js" integrity="sha512-v2CJ7UaYy4JwqLDIrZUI/4hqeoQieOmAZNXBeQyjo21dadnwR+8ZaIJVT8EE2iyI61OV8e6M8PP2/4hpQINQ/g==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 
   <title>User Page</title>
 </head>
@@ -181,12 +175,10 @@ if (!isset($_SESSION['user'])) {
           </button>
 
           <!-- Modal -->
-          <div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog"
-            aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+          <div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
             <div class="modal-dialog modal-dialog-centered" role="document">
               <div class="modal-content">
-                <form action="../Login-Sign page/includes/functions/postsValidation.php" method="post"
-                  enctype="multipart/form-data">
+                <form action="../Login-Sign page/includes/functions/postsValidation.php" method="post" enctype="multipart/form-data">
                   <!-- <form action="" method="" enctype="multipart/form-data"> -->
                   <!-- Header modal -->
                   <div class="modal-header">
@@ -225,8 +217,7 @@ if (!isset($_SESSION['user'])) {
                     <input type="hidden" id="user_token" name="token" value="<?php echo $_SESSION['token'] ?? '' ?>">
                     <div class="num-case">
                       <label for="num-case">رقم القضية:</label>
-                      <input type="number" class="sel-num-case" name="case_Num" id="num-case"
-                        placeholder="ادخل رقم القضية" required />
+                      <input type="number" class="sel-num-case" name="case_Num" id="num-case" placeholder="ادخل رقم القضية" required />
                     </div>
                     <hr style="
                           border: 1px solid #d5d5d5 !important;
@@ -236,8 +227,7 @@ if (!isset($_SESSION['user'])) {
                     <!-- Location Case -->
                     <div class="location-case">
                       <label for="location-case">مكان القضية:</label>
-                      <select class="sel-location-case" name="case_Location" style="width: 78% !important"
-                        id="location-case" required>
+                      <select class="sel-location-case" name="case_Location" style="width: 78% !important" id="location-case" required>
                         <option value="0" selected>اختر مكان القضية</option>
                         <option value="الاسكندرية">الاسكندرية</option>
                         <option value="القاهرة">القاهرة</option>
@@ -271,8 +261,7 @@ if (!isset($_SESSION['user'])) {
                           row-gap: 10px !important;
                         ">
                       <label for="description-case">الموضوع</label>
-                      <textarea name="case_Description" id="description-case" class="description-case" cols="15"
-                        rows="4"></textarea>
+                      <textarea name="case_Description" id="description-case" class="description-case" cols="15" rows="4"></textarea>
                     </div>
                     <hr style="
                           border: 1px solid #d5d5d5 !important;
@@ -282,8 +271,7 @@ if (!isset($_SESSION['user'])) {
                     <!-- Fill Case -->
                     <div class="fill-case">
                       <label for="fill-case">ارفاق ملف للقضية:</label>
-                      <input type="file" class="sel-fill-case" style="width: 70% !important; direction: ltr"
-                        name="case_Fill" id="fill-case" accept="application/pdf, application/vnd.ms-excel" required />
+                      <input type="file" class="sel-fill-case" style="width: 70% !important; direction: ltr" name="case_Fill" id="fill-case" accept="application/pdf, application/vnd.ms-excel" required />
                     </div>
                     <!-- <hr
                       style="border: 1px solid #d5d5d5 !important; width: 100%"
@@ -291,8 +279,7 @@ if (!isset($_SESSION['user'])) {
                     <!-- Image Case -->
                     <div class="image-case mt-3">
                       <label for="image-case">ارفاق صور للقضية:</label>
-                      <input type="file" class="sel-image-case" style="width: 70% !important; direction: ltr"
-                        name="case_Image" id="image-case" accept="image/gif, image/jpeg" required />
+                      <input type="file" class="sel-image-case" style="width: 70% !important; direction: ltr" name="case_Image" id="image-case" accept="image/gif, image/jpeg" required />
                     </div>
                   </div>
                   <!-- Footer Modal -->
@@ -590,128 +577,7 @@ if (!isset($_SESSION['user'])) {
           <!-- My Postes -->
           <div class="postes">
             <div class="postes-wrapper">
-              <div class="post">
-                <div class="header-post">
-                  <div class="profile">
-                    <a href="profileUser.html">
-                      <img src="asstesUser/lawyer3.jpg" alt="user" /></a>
-                  </div>
-                  <div class="name-user">
-                    <h1>احمد احمد احمد</h1>
-                  </div>
-                </div>
-                <hr style="
-                      margin: 0.5rem !important;
-                      border: 1px solid #d5d5d5 !important;
-                      width: 100%;
-                    " />
-                <div class="content-post">
-                  <div class="content-header">
-                    <!-- type case -->
-                    <div class="post-type-case">
-                      <h3>نوع القضية:</h3>
-                      <span>تجاري و شركات .</span>
-                    </div>
-                    <!-- location case -->
-                    <div class="post-location-case">
-                      <h3>مكان القضية:</h3>
-                      <span>الاسكندرية .</span>
-                    </div>
-                  </div>
-                  <!-- date case -->
-                  <div class="post-date-case">
-                    <div class="date-dm">
-                      <h3>تاريخ القضية:</h3>
-                      <span>01/12/2024</span>
-                    </div>
-                    <span><strong>-</strong></span>
-                    <div class="date-houre">
-                      <h3>الميعاد:</h3>
-                      <span>3:30</span>
-                    </div>
-                  </div>
-                  <hr style="
-                        margin: 0.5rem !important;
-                        border: 1px solid #d5d5d5 !important;
-                        width: 100%;
-                      " />
-                  <!-- description case -->
-                  <div class="post-description-case">
-                    <p>
-                      تقوم جريمة السرقة فى حق مرتكبيها بتوافر القصد الجنائي فى
-                      الفعل المؤثم؛بعلمه وقت ارتكاب الفعل بأنه يختلس منقولًا
-                      مملوكًا للغير من غير رضاه بنية تملكه. وتختلف جريمة
-                      السرقة العادية عن جريمة السرقة بالإكراه، إذ أن جريمة
-                      السرقة تكون عن طريق الغفلة، أما جريمة السرقة بالإكراه
-                      يُستخدم فيها إحدى أدوات الإكراه لسرقة المجني عليه.
-                    </p>
-                  </div>
-                </div>
-                <div class="footer-post">
-                  <span>الغاء القضية</span>
-                </div>
-              </div>
-              <div class="post">
-                <div class="header-post">
-                  <div class="profile">
-                    <a href="profileUser.html">
-                      <img src="asstesUser/lawyer3.jpg" alt="user" /></a>
-                  </div>
-                  <div class="name-user">
-                    <h1>احمد احمد احمد</h1>
-                  </div>
-                </div>
-                <hr style="
-                      margin: 0.5rem !important;
-                      border: 1px solid #d5d5d5 !important;
-                      width: 100%;
-                    " />
-                <div class="content-post">
-                  <div class="content-header">
-                    <!-- type case -->
-                    <div class="post-type-case">
-                      <h3>نوع القضية:</h3>
-                      <span>تجاري و شركات .</span>
-                    </div>
-                    <!-- location case -->
-                    <div class="post-location-case">
-                      <h3>مكان القضية:</h3>
-                      <span>الاسكندرية .</span>
-                    </div>
-                  </div>
-                  <!-- date case -->
-                  <div class="post-date-case">
-                    <div class="date-dm">
-                      <h3>تاريخ القضية:</h3>
-                      <span>01/12/2024</span>
-                    </div>
-                    <span><strong>-</strong></span>
-                    <div class="date-houre">
-                      <h3>الميعاد:</h3>
-                      <span>3:30</span>
-                    </div>
-                  </div>
-                  <hr style="
-                        margin: 0.5rem !important;
-                        border: 1px solid #d5d5d5 !important;
-                        width: 100%;
-                      " />
-                  <!-- description case -->
-                  <div class="post-description-case">
-                    <p>
-                      تقوم جريمة السرقة فى حق مرتكبيها بتوافر القصد الجنائي فى
-                      الفعل المؤثم؛بعلمه وقت ارتكاب الفعل بأنه يختلس منقولًا
-                      مملوكًا للغير من غير رضاه بنية تملكه. وتختلف جريمة
-                      السرقة العادية عن جريمة السرقة بالإكراه، إذ أن جريمة
-                      السرقة تكون عن طريق الغفلة، أما جريمة السرقة بالإكراه
-                      يُستخدم فيها إحدى أدوات الإكراه لسرقة المجني عليه.
-                    </p>
-                  </div>
-                </div>
-                <div class="footer-post">
-                  <span>الغاء القضية</span>
-                </div>
-              </div>
+              <!-- Post Case -->
               <div class="post">
                 <div class="header-post">
                   <div class="profile">
@@ -935,76 +801,86 @@ if (!isset($_SESSION['user'])) {
   <script src="./Scripts/userPage.js"></script>
 
   <script>
-    $(document).ready(function () {
+    $(document).ready(function() {
 
       $.getJSON({
         url: 'userApi/userApi.php',
         type: "GET",
         // data: data,
-        success: function (data) {
+        success: function(data) {
           console.log("data", data.posts);
           console.log("data", data.userData);
           // console.log("GET", data);
+          var post = data.posts;
+          var user = data.userData;
+          console.log("post", post)
+
+          $(post).each((val, el) => {
+            console.log("val", val)
+            console.log("el", el)
+            var NewPost = `<div class="post">
+                <div class="header-post">
+                  <div class="profile">
+                    <a href="profileUser.html">
+                      <img src="asstesUser/lawyer3.jpg" alt="user" /></a>
+                  </div>
+                  <div class="name-user">
+                    <h1>احمد احمد احمد</h1>
+                  </div>
+                </div>
+                <hr style="
+                      margin: 0.5rem !important;
+                      border: 1px solid #d5d5d5 !important;
+                      width: 100%;
+                    " />
+                <div class="content-post">
+                  <div class="content-header">
+                    <!-- type case -->
+                    <div class="post-type-case">
+                      <h3>نوع القضية:</h3>
+                      <span>${el.typeCase} .</span>
+                    </div>
+                    <div class="post-location-case">
+                      <h3>مكان القضية:</h3>
+                      <span>${el.location} .</span>
+                    </div>
+                  </div>
+                  <div class="post-date-case">
+                    <div class="date-dm">
+                      <h3>تاريخ القضية:</h3>
+                      <span>${el.Date}</span>
+                    </div>
+                    <span><strong>-</strong></span>
+                    <div class="date-houre">
+                      <h3>الميعاد:</h3>
+                      <span>${el.houerCase.slice(0,5)}</span>
+                    </div>
+                  </div>
+                  <hr style="
+                        margin: 0.5rem !important;
+                        border: 1px solid #d5d5d5 !important;
+                        width: 100%;
+                      " />
+                  <div class="post-description-case">
+                    <p>
+                      ${el.description}.
+                    </p>
+                  </div>
+                </div>
+                <div class="footer-post">
+                  <span>الغاء القضية</span>
+                </div>
+              </div>`;
+
+            $(".postes-wrapper").append(NewPost);
+          })
+
+
         }
       })
 
 
 
-      // $(".btn-add-case").click(() => {
-      //           var token_post = $("#user_token").val();
-      //           var type_case = $("#type-case").val();
-      //           var num_case = $("#num-case").val();
-      //           var location_case = $("#location-case").val();
-      //           var date_case = $("#date-case").val();
-      //           var hour_case = $("#hour-case").val();
-      //           var description_case = $("#description-case").val();
-      //           var fill_case = $("#fill-case")[0].files[0];
-      //           var image_case = $("#image-case")[0].files[0];
-
-      //           var data_obj = {
-      //             case_Token: token_post,
-      //             case_Type: type_case,
-      //             case_Num: JSON.parse(num_case),
-      //             case_Location: location_case,
-      //             case_Date: date_case,
-      //             case_Hour: hour_case,
-      //             case_Description: description_case,
-      //             case_Fill: JSON.stringify(fill_case),
-      //             case_Image: image_case,
-      //           };
-      //           console.log(data_obj)
-
-      //           $.ajaxSetup({
-      //             headers: {
-      //               'CSRF-TOKEN': $('#user_token').val(),
-      //             }
-      //           });
-      //           $.ajax({
-      //             type: "POST",
-      //             url: '../Login-Sign page/includes/functions/postsValidation.php',
-      //             data: {
-      //               data: JSON.stringify(data_obj)
-      //             }, // now data come in this function
-      //             // processData: false,
-      //             // contentType: false,
-      //             // crossDomain: true,
-      //             dataType: "json",
-      //             success: function (data, status, jqXHR) {
-
-      //               alert("success");
-      //             },
-
-      //             error: function (data, jqXHR, status) {
-      //               // error handler
-      //               alert('fail' + status.code);
-      //             }
-      //           });
-
-
-      //           // console.log(data)
-
-
-      // });
     })
   </script>
 </body>
