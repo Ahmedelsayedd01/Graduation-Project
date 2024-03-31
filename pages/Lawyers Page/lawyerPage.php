23c81613000eee4667abc438de51a717ac02045c
ahmedelsayedd01 <ahmedelsayed201972@gmail.com>
2024-03-31 02:58:47 +0200

    Fetch posts Case A

--- pages/Lawyers Page/lawyerPage.php
+++ pages/Lawyers Page/lawyerPage.php
@@ -22,21 +22,15 @@ if (!isset($_SESSION['user'])) {
   <!-- Icons Liberary -->
   <script src="https://kit.fontawesome.com/bbda8ae88d.js" crossorigin="anonymous"></script>
   <!-- Bootstrap Liberary -->
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
-    integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous" />
-  <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
-    integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous" />
+  <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous">
   </script>
-  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js"
-    integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous">
+  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous">
   </script>
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js"
-    integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous">
   </script>
   <!-- Jquery Liberary -->
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js"
-    integrity="sha512-v2CJ7UaYy4JwqLDIrZUI/4hqeoQieOmAZNXBeQyjo21dadnwR+8ZaIJVT8EE2iyI61OV8e6M8PP2/4hpQINQ/g=="
-    crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js" integrity="sha512-v2CJ7UaYy4JwqLDIrZUI/4hqeoQieOmAZNXBeQyjo21dadnwR+8ZaIJVT8EE2iyI61OV8e6M8PP2/4hpQINQ/g==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 
   <title>Lawyer Page</title>
 </head>
@@ -444,190 +438,6 @@ if (!isset($_SESSION['user'])) {
           <div class="postes">
             <div class="postes-wrapper">
               <!-- Post -->
-              <div class="post">
-                <div class="header-post">
-                  <div class="profile">
-                    <img src="asstesLawyer/user.jpg" alt="user" />
-                  </div>
-                  <div class="name-user">
-                    <h1>احمد احمد احمد</h1>
-                  </div>
-                </div>
-                <hr style="
-                      margin: 0.5rem !important;
-                      border: 1px solid #d5d5d5 !important;
-                      width: 100%;
-                    " />
-                <div class="content-post">
-                  <div class="content-header">
-                    <!-- type case -->
-                    <div class="post-type-case">
-                      <h3>نوع القضية:</h3>
-                      <span>تجاري و شركات .</span>
-                    </div>
-                    <!-- location case -->
-                    <div class="post-location-case">
-                      <h3>مكان القضية:</h3>
-                      <span>الاسكندرية .</span>
-                    </div>
-                  </div>
-                  <!-- date case -->
-                  <div class="post-date-case">
-                    <div class="date-dm">
-                      <h3>تاريخ القضية:</h3>
-                      <span>01/12/2024</span>
-                    </div>
-                    <span><strong>-</strong></span>
-                    <div class="date-houre">
-                      <h3>الميعاد:</h3>
-                      <span>3:30</span>
-                    </div>
-                  </div>
-                  <hr style="
-                        margin: 0.5rem !important;
-                        border: 1px solid #d5d5d5 !important;
-                        width: 100%;
-                      " />
-                  <!-- description case -->
-                  <div class="post-description-case">
-                    <p>
-                      تقوم جريمة السرقة فى حق مرتكبيها بتوافر القصد الجنائي فى
-                      الفعل المؤثم؛بعلمه وقت ارتكاب الفعل بأنه يختلس منقولًا
-                      مملوكًا للغير من غير رضاه بنية تملكه. وتختلف جريمة
-                      السرقة العادية عن جريمة السرقة بالإكراه، إذ أن جريمة
-                      السرقة تكون عن طريق الغفلة، أما جريمة السرقة بالإكراه
-                      يُستخدم فيها إحدى أدوات الإكراه لسرقة المجني عليه.
-                    </p>
-                  </div>
-                </div>
-                <div class="footer-post">
-                  <a>طلب القضية</a>
-                </div>
-              </div>
-              <!-- Post -->
-              <div class="post">
-                <div class="header-post">
-                  <div class="profile">
-                    <a href="profileUser.html">
-                      <img src="asstesLawyer/user.jpg" alt="user" /></a>
-                  </div>
-                  <div class="name-user">
-                    <h1>احمد احمد احمد</h1>
-                  </div>
-                </div>
-                <hr style="
-                      margin: 0.5rem !important;
-                      border: 1px solid #d5d5d5 !important;
-                      width: 100%;
-                    " />
-                <div class="content-post">
-                  <div class="content-header">
-                    <!-- type case -->
-                    <div class="post-type-case">
-                      <h3>نوع القضية:</h3>
-                      <span>تجاري و شركات .</span>
-                    </div>
-                    <!-- location case -->
-                    <div class="post-location-case">
-                      <h3>مكان القضية:</h3>
-                      <span>الاسكندرية .</span>
-                    </div>
-                  </div>
-                  <!-- date case -->
-                  <div class="post-date-case">
-                    <div class="date-dm">
-                      <h3>تاريخ القضية:</h3>
-                      <span>01/12/2024</span>
-                    </div>
-                    <span><strong>-</strong></span>
-                    <div class="date-houre">
-                      <h3>الميعاد:</h3>
-                      <span>3:30</span>
-                    </div>
-                  </div>
-                  <hr style="
-                        margin: 0.5rem !important;
-                        border: 1px solid #d5d5d5 !important;
-                        width: 100%;
-                      " />
-                  <!-- description case -->
-                  <div class="post-description-case">
-                    <p>
-                      تقوم جريمة السرقة فى حق مرتكبيها بتوافر القصد الجنائي فى
-                      الفعل المؤثم؛بعلمه وقت ارتكاب الفعل بأنه يختلس منقولًا
-                      مملوكًا للغير من غير رضاه بنية تملكه. وتختلف جريمة
-                      السرقة العادية عن جريمة السرقة بالإكراه، إذ أن جريمة
-                      السرقة تكون عن طريق الغفلة، أما جريمة السرقة بالإكراه
-                      يُستخدم فيها إحدى أدوات الإكراه لسرقة المجني عليه.
-                    </p>
-                  </div>
-                </div>
-                <div class="footer-post">
-                  <a>طلب القضية</a>
-                </div>
-              </div>
-              <!-- Post -->
-              <div class="post">
-                <div class="header-post">
-                  <div class="profile">
-                    <a href="profileUser.html">
-                      <img src="asstesLawyer/user.jpg" alt="user" /></a>
-                  </div>
-                  <div class="name-user">
-                    <h1>احمد احمد احمد</h1>
-                  </div>
-                </div>
-                <hr style="
-                      margin: 0.5rem !important;
-                      border: 1px solid #d5d5d5 !important;
-                      width: 100%;
-                    " />
-                <div class="content-post">
-                  <div class="content-header">
-                    <!-- type case -->
-                    <div class="post-type-case">
-                      <h3>نوع القضية:</h3>
-                      <span>تجاري و شركات .</span>
-                    </div>
-                    <!-- location case -->
-                    <div class="post-location-case">
-                      <h3>مكان القضية:</h3>
-                      <span>الاسكندرية .</span>
-                    </div>
-                  </div>
-                  <!-- date case -->
-                  <div class="post-date-case">
-                    <div class="date-dm">
-                      <h3>تاريخ القضية:</h3>
-                      <span>01/12/2024</span>
-                    </div>
-                    <span><strong>-</strong></span>
-                    <div class="date-houre">
-                      <h3>الميعاد:</h3>
-                      <span>3:30</span>
-                    </div>
-                  </div>
-                  <hr style="
-                        margin: 0.5rem !important;
-                        border: 1px solid #d5d5d5 !important;
-                        width: 100%;
-                      " />
-                  <!-- description case -->
-                  <div class="post-description-case">
-                    <p>
-                      تقوم جريمة السرقة فى حق مرتكبيها بتوافر القصد الجنائي فى
-                      الفعل المؤثم؛بعلمه وقت ارتكاب الفعل بأنه يختلس منقولًا
-                      مملوكًا للغير من غير رضاه بنية تملكه. وتختلف جريمة
-                      السرقة العادية عن جريمة السرقة بالإكراه، إذ أن جريمة
-                      السرقة تكون عن طريق الغفلة، أما جريمة السرقة بالإكراه
-                      يُستخدم فيها إحدى أدوات الإكراه لسرقة المجني عليه.
-                    </p>
-                  </div>
-                </div>
-                <div class="footer-post">
-                  <a>طلب القضية</a>
-                </div>
-              </div>
             </div>
           </div>
         </div>
@@ -844,113 +654,82 @@ if (!isset($_SESSION['user'])) {
   <script src="./Scripts/lawyerPage.js"></script>
 
   <script>
-    $(document).ready(function () {
+    $(document).ready(function() {
 
       $.getJSON({
         url: 'userApi/userApi.php',
         type: "GET",
         // data: data,
-        success: function (data) {
+        success: function(data) {
           console.log("data", data.posts);
           console.log("data", data.userData);
           // console.log("GET", data);
+          var post = data.posts;
+          var user = data.userData;
+          // test Log And Append Image Posts
+          // console.log("Image Start", post[0].ImageCase)
+          // console.log("Image Start", post[0].fileCase)
+
+          $(post).each((val, el) => {
+            var NewPost = `<div class="post">
+                <div class="header-post">
+                  <div class="profile">
+                    <a href="profileUser.html">
+                      <img src="asstesLawyer/user.jpg" alt="user" /></a>
+                  </div>
+                  <div class="name-user">
+                    <h1>${user.userName +" "+ user.lastName}</h1>
+                  </div>
+                </div>
+                <hr style="
+                      margin: 0.5rem !important;
+                      border: 1px solid #d5d5d5 !important;
+                      width: 100%;
+                    " />
+                <div class="content-post">
+                  <div class="content-header">
+                    <!-- type case -->
+                    <div class="post-type-case">
+                      <h3>نوع القضية:</h3>
+                      <span>${el.typeCase} .</span>
+                    </div>
+                    <div class="post-location-case">
+                      <h3>مكان القضية:</h3>
+                      <span>${el.location} .</span>
+                    </div>
+                  </div>
+                  <div class="post-date-case">
+                    <div class="date-dm">
+                      <h3>تاريخ القضية:</h3>
+                      <span>${el.Date}</span>
+                    </div>
+                    <span><strong>-</strong></span>
+                    <div class="date-houre">
+                      <h3>الميعاد:</h3>
+                      <span>${el.houerCase.slice(0,5)}</span>
+                    </div>
+                  </div>
+                  <hr style="
+                        margin: 0.5rem !important;
+                        border: 1px solid #d5d5d5 !important;
+                        width: 100%;
+                      " />
+                  <div class="post-description-case">
+                    <p>
+                      ${el.description}
+                    </p>
+                  </div>
+                </div>
+                <div class="footer-post">
+                  <a>طلب القضية</a>
+                </div>
+              </div>`;
+
+            $(".postes-wrapper").append(NewPost);
+          })
         }
       })
 
-      var desName = $("#description-case");
-      var btnS = $("#send");
-      var chosen = $("#send");
-      var uploadimage = $("#image-new");
-
-
-      function upload() {
-        uploadimage.click()
-      }
-
-      $(chosen).click(upload)
-
-      $(uploadimage).change(function () {
-        var file = $(this)[0].files[0];
-        console.log(file)
-
-        if ($(desName).val() == "") {
-          $(desName).val(file.name)
-        }
-        $(chosen).text(`you good${file.name} picture`)
-      })
-
-
-
-      $("#form_post").submit((event) => {
-        event.preventDefault();
-
-        // var myForm = $("#form_post");
-
-        // var inputImg = $("#image-case");
-        // var inputFile = $("#fill-case");
-
-        // var formData = new FormData();
-        // formData.append("files", inputFile[0].files[0]);
-
-        var token_post = $("#user_token").val();
-        var type_case = $("#type-case").val();
-        var num_case = $("#num-case").val();
-        var location_case = $("#location-case").val();
-        var date_case = $("#date-case").val();
-        var hour_case = $("#hour-case").val();
-        var description_case = $("#description-case").val();
-        var fill_case = $("#fill-case")[0].files[0];
-        var image_case = $("#image-case")[0].files[0];
-
-        var data_obj = {
-          case_Token: token_post,
-          case_Type: type_case,
-          case_Num: JSON.parse(num_case),
-          case_Location: location_case,
-          case_Date: date_case,
-          case_Hour: hour_case,
-          case_Description: description_case,
-          case_Fill: fill_case.name,
-          case_Image: image_case,
-          // case_Fill: formData,
-          // case_Image: image_case,
-        };
-        console.log(data_obj)
-
-        $.ajaxSetup({
-          headers: {
-            'CSRF-TOKEN': $('#user_token').val(),
-            // "Content-Type": "multipart/form-data",
-          }
-        });
-        $.ajax({
-          type: "POST",
-          url: '../Login-Sign page/includes/functions/postsValidation.php',
-          data: {
-            data: JSON.stringify(data_obj),
-            // case_Fill: formData,
-            // case_Image: image_case,
-          }, // now data come in this function
-          // processData: false,
-          // contentType: false,
-          // crossDomain: true,
-          dataType: "json",
-          success: function (data, status, jqXHR) {
-
-            alert("success");
-          },
-
-          error: function (data, jqXHR, status) {
-            // error handler
-            alert('fail' + status.code);
-          }
-        });
-
-
-        // console.log(data)
-
-
-      });
     })
   </script>
 </body>
