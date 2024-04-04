f2ec37088e4418c98fda364facd8833c227e31f8
Ziad <89700774+ZxProgramming@users.noreply.github.com>
2024-04-04 02:53:58 +0200

    End posts lawyer

--- pages/Lawyers Page/lawyerApi/lawyerApi.php
+++ pages/Lawyers Page/lawyerApi/lawyerApi.php
@@ -2,14 +2,17 @@
 session_start();
 if(isset($_SESSION['lawyer'])){
   include('../../Login-Sign page/includes/functions/functions.php');
+  $sql = $con->prepare( "SELECT * FROM posts LEFT JOIN users ON posts.user_id = users.id WHERE users.Role='user'");
+  $sql->execute();
+  $posts = $sql->fetchAll();
   $lawyers_id = $_SESSION['lawyer']['id']; // Take The Id In Session
   $lawyers= selectAllData('*','lawyer'); //Select Data Where id = lawyers_id
-  $posts= selectAllData('*','posts'); //Select Data Where id = lawyers_id
   $lawyers = json_encode(
     [
       'lawyersData' => $_SESSION['lawyer'], // This Daata Where lawyers Login
       'posts'=>$posts,                 // This is Data Posts From lawyers
       'lawyers'=>$lawyers, // This is Data Posts From lawyers
+      'data'=>$posts, // This is Data Posts From lawyers
     ],
     200); // Data Send Successflly 
 
