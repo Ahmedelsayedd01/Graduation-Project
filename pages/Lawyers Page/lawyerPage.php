b58c881fdb10388e05a1c4049a7a89d4633abd2e
ahmedelsayedd01 <ahmedelsayed201972@gmail.com>
2024-04-20 22:41:26 +0200

    new A

--- pages/Lawyers Page/lawyerPage.php
+++ pages/Lawyers Page/lawyerPage.php
@@ -477,7 +477,7 @@ if (!isset($_SESSION['lawyer'])) {
               <div class="header-bestLawyer">
                 <!-- Photo lawyer Request -->
                 <div class="profile">
-                  <a href="profileLawyer.php">
+                  <a href="profileLawyer.php/${ele.id}">
                     <img src="asstesLawyer/lawyer3.jpg" alt="lawyer" /></a>
                 </div>
                 <!-- Details lawyer Request -->
